e589f195889eb7db390f93b420c311ef777e7c36
shaktip258 <shaktip258@gmail.com>
2018-04-18 15:52:33 -0400

    cleanup

--- app/src/main/java/shaxapps/grocerymate/Login.java
+++ app/src/main/java/shaxapps/grocerymate/Login.java
@@ -1,4 +1,13 @@
 package shaxapps.grocerymate;
 
-public class Login {
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Login extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
 }
